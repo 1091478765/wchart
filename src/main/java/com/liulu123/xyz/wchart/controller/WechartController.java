818600f77aa7165a5304e2eb83af2ecb8a174b64
liulu <1091478765@qq.com>
2019-05-23 19:48:01 +0800

    修改验证方法

--- src/main/java/com/liulu123/xyz/wchart/controller/WechartController.java
+++ src/main/java/com/liulu123/xyz/wchart/controller/WechartController.java
@@ -1,16 +1,27 @@
 package com.liulu123.xyz.wchart.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/wx/")
 public class WechartController {
 
-    @GetMapping("/test")
-    public String test5(){
-        return "这是远程api服务";
+    private static Logger logger = LoggerFactory.getLogger(WechartController.class);
+
+    @RequestMapping("/verifyWxToken")
+    public String verifyWxToken(HttpServletRequest request){
+        logger.info("进入验证方法");
+        String msgSignature = request.getParameter("signature");
+        String msgTimestamp = request.getParameter("timestamp");
+        String msgNonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        logger.info("进入验证方法echostr为{}",echostr);
+        return echostr;
     }
 }
