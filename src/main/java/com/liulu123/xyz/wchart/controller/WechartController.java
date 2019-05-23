e4d71f96790f7d6901b46ecc20bc5cf6aa16d322
liulu <1091478765@qq.com>
2019-05-24 00:06:16 +0800

    微信配置

--- src/main/java/com/liulu123/xyz/wchart/controller/WechartController.java
+++ src/main/java/com/liulu123/xyz/wchart/controller/WechartController.java
@@ -3,9 +3,11 @@ package com.liulu123.xyz.wchart.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,6 +16,12 @@ import javax.servlet.http.HttpServletRequest;
 public class WechartController {
 
     private static Logger logger = LoggerFactory.getLogger(WechartController.class);
+
+    @Value("${wechart.appID}")
+    private String appID;
+
+    @Value("${wechart.appsecret}")
+    private String appsecret;
 
     @RequestMapping("verifyWxToken")
     public String verifyWxToken(HttpServletRequest request){
@@ -29,5 +37,22 @@ public class WechartController {
     @RequestMapping("auth")
     public void auth(@RequestParam(value = "code",required = false) String code){
         logger.info("进入授权认证方法,code为:{}",code);
+        StringBuffer urlBuff = new StringBuffer("https://api.weixin.qq.com/sns/oauth2/access_token?");
+        urlBuff
+                .append("appid=")
+                .append(appID)
+                .append("&")
+                .append("secret=")
+                .append(appsecret)
+                .append("&")
+                .append("code=")
+                .append(code)
+                .append("&")
+                .append("grant_type=authorization_code");
+        RestTemplate restTemplate= new RestTemplate();
+        logger.info("请求url为{}",urlBuff.toString());
+        String forObject = restTemplate.getForObject(urlBuff.toString(), String.class);
+        logger.info("返回结果为{}",forObject);
+        //https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code
     }
 }
